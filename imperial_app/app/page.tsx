e3d27731224ea3f7c63d8c212978b5f07c0f73ed
darkcom109 <garciavalentino2006@gmail.com>
2025-10-17 01:09:43 +0100

    Attempted to fix issue with label

--- imperial_app/app/page.tsx
+++ imperial_app/app/page.tsx
@@ -242,7 +242,6 @@ export default function Home() {
                     cx="50%"
                     cy="50%"
                     labelLine={false}
-                    label={({ name, percent }: { name: string; percent: number }) => `${name} ${(percent * 100).toFixed(0)}%`}
                     outerRadius={80}
                     fill="#8884d8"
                     dataKey="value"
